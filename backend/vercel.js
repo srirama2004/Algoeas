dd66b0c50e1de4d5667d5cc4cf398e853f5c9260
srirama <sriramavate@gmail.com>
2025-03-10 23:16:35 +0530

    f3

---
+++ backend/vercel.js
@@ -0,0 +1,22 @@
+{
+    "headers": [
+      {
+        "source": "/(.*)",
+        "headers": [
+          {
+            "key": "Access-Control-Allow-Origin",
+            "value": "https://algoeas-frontend.vercel.app"
+          },
+          {
+            "key": "Access-Control-Allow-Methods",
+            "value": "GET, POST, PUT, DELETE, OPTIONS"
+          },
+          {
+            "key": "Access-Control-Allow-Headers",
+            "value": "Content-Type, Authorization"
+          }
+        ]
+      }
+    ]
+  }
+  
